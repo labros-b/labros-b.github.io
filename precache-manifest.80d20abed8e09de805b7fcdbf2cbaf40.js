a67a0e15f11a4ef6a9b86de87d6795eb9b643fd9
labros-b <labrosb@gmail.com>
2024-12-16 17:07:06 +0200

    Updates

---
+++ precache-manifest.80d20abed8e09de805b7fcdbf2cbaf40.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4c54e40109b4712fd77ae2f3358101",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dc250816ad8f4a535a0",
+    "url": "/static/css/main.77e9d511.chunk.css"
+  },
+  {
+    "revision": "2fc96c3f83e03e2f1181",
+    "url": "/static/js/2.372fd32e.chunk.js"
+  },
+  {
+    "revision": "6dc250816ad8f4a535a0",
+    "url": "/static/js/main.53146bab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9826806f00019f590888f4e49cc60176",
+    "url": "/static/media/avatar.9826806f.png"
+  },
+  {
+    "revision": "c05290efe89adfa86fbda6ad521073d4",
+    "url": "/static/media/background-auroras.c05290ef.jpg"
+  },
+  {
+    "revision": "c37f0aac96611a922bec894b4f5775ea",
+    "url": "/static/media/background-main.c37f0aac.jpg"
+  },
+  {
+    "revision": "df115dfd286175c1e39b2978adbb6201",
+    "url": "/static/media/background-space.df115dfd.jpg"
+  },
+  {
+    "revision": "2b887161893348e3345b70c63055002e",
+    "url": "/static/media/background-university.2b887161.jpg"
+  },
+  {
+    "revision": "53d387c87a09de16d75bc2a6dc462f7f",
+    "url": "/static/media/brick_breaker_image.53d387c8.png"
+  },
+  {
+    "revision": "2c0862a67bfb8773d52c7833e17cd5ca",
+    "url": "/static/media/chevron-down-solid.2c0862a6.svg"
+  },
+  {
+    "revision": "e8e8f759ef0ad9a3fd65a2a7cb460431",
+    "url": "/static/media/code-solid.e8e8f759.svg"
+  },
+  {
+    "revision": "33b58bba53ad5b1c24a88a11a406024b",
+    "url": "/static/media/flying_dutchman_image.33b58bba.png"
+  },
+  {
+    "revision": "8ac4a039749fb88f233726e75fa84ff8",
+    "url": "/static/media/geek-boy-laptop.8ac4a039.png"
+  },
+  {
+    "revision": "763e29488426321dd6a2124acdd1ebbc",
+    "url": "/static/media/geek-boy-pointing-rev.763e2948.png"
+  },
+  {
+    "revision": "a24e92d4fd83e9571b492d51a04aca9b",
+    "url": "/static/media/geek-boy-pointing.a24e92d4.png"
+  },
+  {
+    "revision": "4e73cdcfa4c84983af5a3a9506dbca7e",
+    "url": "/static/media/githubFinder.4e73cdcf.jpg"
+  },
+  {
+    "revision": "e274581238709fcd099e88af0eb57087",
+    "url": "/static/media/google-play.e2745812.svg"
+  },
+  {
+    "revision": "b9d22e5e62549347d00481caac75f999",
+    "url": "/static/media/hospital_image.b9d22e5e.png"
+  },
+  {
+    "revision": "ccdebdb97ed614de447aa3122bb56e05",
+    "url": "/static/media/monkeyMedPhone.ccdebdb9.png"
+  },
+  {
+    "revision": "f1580cd94e6e1bb774e6a8350e1b6ae3",
+    "url": "/static/media/netandget-coupons.f1580cd9.jpg"
+  },
+  {
+    "revision": "d67c32c813ffacd36a992a2d77ed0c05",
+    "url": "/static/media/netandget-marketplace.d67c32c8.png"
+  },
+  {
+    "revision": "ca01be554e5bb6135b6434bbfa601df5",
+    "url": "/static/media/play-circle-solid.ca01be55.svg"
+  },
+  {
+    "revision": "85fd193999d9b684cf8d0ee4144abf8e",
+    "url": "/static/media/prs.85fd1939.jpg"
+  },
+  {
+    "revision": "e7b3bd64888e3579f95a03dd8b302439",
+    "url": "/static/media/thumbs.e7b3bd64.png"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/static/media/times-solid.2c5552a1.svg"
+  },
+  {
+    "revision": "9fa275b564557ff5fc0510ca47eb9f86",
+    "url": "/static/media/welcome.9fa275b5.png"
+  },
+  {
+    "revision": "4fcdd0ab05d721aa92d6d5b6c5e820b9",
+    "url": "/static/media/youtube.4fcdd0ab.png"
+  },
+  {
+    "revision": "c80ca203c0f773a8502d85ae60f347ce",
+    "url": "/static/media/youtube.c80ca203.svg"
+  }
+]);
